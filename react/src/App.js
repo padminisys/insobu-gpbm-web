683869f1c8069d8b1727bed651a734e6fcc07f51
ramanujd <ramanuj@padminisys.com>
2021-11-10 02:32:46 +0530

    ui changes for pipeline

--- react/src/App.js
+++ react/src/App.js
@@ -8,11 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+          <Button type="primary">Click Me</Button>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>AWS powers deployment of this application.</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
