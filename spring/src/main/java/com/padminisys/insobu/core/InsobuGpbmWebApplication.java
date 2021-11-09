b5f2a5e2275602e3eec98f1cef1f05018861c780
ramanujd <ramanuj@padminisys.com>
2021-11-09 16:28:40 +0530

    Added Spring and React build directory path set to serve static content

---
+++ spring/src/main/java/com/padminisys/insobu/core/InsobuGpbmWebApplication.java
@@ -0,0 +1,13 @@
+package com.padminisys.insobu.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsobuGpbmWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsobuGpbmWebApplication.class, args);
+	}
+
+}
