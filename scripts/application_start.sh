ad39c8ad0a63191f4623e09a13b5abd8b78c323d
ramanujd <ramanuj@padminisys.com>
2021-12-03 23:01:16 +0530

    post request for shutdown

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,1 +1,2 @@
-java -jar /tmp/insobu-gpbm-web-0.0.1-SNAPSHOT.jar >log.txt &
+curl -X POST -H "Content-Type: application/json" -d @req.json http://localhost/actuator/shutdown
+sudo java -jar /tmp/insobu-gpbm-web-0.0.1-SNAPSHOT.jar > $HOME/log.txt &
