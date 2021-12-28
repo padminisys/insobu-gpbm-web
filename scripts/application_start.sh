e2712d4aa35d6ce082e2478686794fc51a3ad781
ramanujd <ramanuj@padminisys.com>
2021-12-28 11:47:41 +0530

    minor x

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,2 @@
 curl -X POST -H "Content-Type: application/json" -d @req.json http://localhost/actuator/shutdown
-sudo java -jar /tmp/insobu-gpbm-web-0.0.1-SNAPSHOT.jar > $HOME/log.txt
+sudo java -jar /tmp/insobu-gpbm-web-0.0.1-SNAPSHOT.jar > $HOME/log.txt &
